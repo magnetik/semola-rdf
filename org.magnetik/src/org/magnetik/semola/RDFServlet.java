b3bbd2b28d1702270d1c3df0aa5866eda1cae328
Baptiste Lafontaine <baptiste33@gmail.com>
2011-12-08 14:42:13 +0100

    Forgot to add code..

---
+++ org.magnetik/src/org/magnetik/semola/RDFServlet.java
@@ -0,0 +1,42 @@
+package org.magnetik.semola;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+/**
+ * Jena Libs should be placed in war/WEB-INF/lib/ directory
+ */
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+public class RDFServlet extends HttpServlet {
+	private static final long serialVersionUID = -5485387849338180912L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().println("Nothing to see here");
+    }
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String input, inputType, outputType, content;
+		
+		Model model = ModelFactory.createDefaultModel();
+		
+		input = req.getParameter("input");
+		outputType = req.getParameter("outputType");
+		inputType = req.getParameter("inputType");
+		
+		if ((input == null) || (input == null) || (input == null)) {
+			resp.sendError(500, "You should provide : input, inputType, outputType");
+		}
+		//TODO : check outputType
+		else {
+			System.out.println("Converting input from " + inputType + " to " + outputType);
+			model.read(new ByteArrayInputStream(input.getBytes()), null, inputType);
+			model.write(resp.getWriter(), outputType);
+		}
+		
+	}
+}
